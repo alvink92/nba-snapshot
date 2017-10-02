54ad77f3be6d31810c97adeb74dade105110cfff
Alvin Kong <alvink92@gmail.com>
2017-10-02 09:37:35 -0700

    configed webpack and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,23 @@ module.exports = {
   entry: "./lib/main.js",
   output: {
     path: path.resolve(__dirname),
-    filename: "bundle.js"
+    filename: "./lib/bundle.js"
   },
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: [/\.js?$/],
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
-          presets: ["es2015", "react"]
+          presets: ["es2015"]
         }
       }
     ]
   },
   devtool: "source-maps",
   resolve: {
-    extensions: [".js", ".jsx", "*"]
+    extensions: [".js", "*"]
   },
 
   plugins: [
